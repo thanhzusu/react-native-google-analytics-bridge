81037603883881e16b5cb7d400db3e11ab58ebcc
Thanh Le <>
2018-06-06 15:47:09 +0700

    fixed conflict firebase version issue

--- android/src/main/java/com/idehub/GoogleAnalyticsBridge/GoogleAnalyticsBridgePackage.java
+++ android/src/main/java/com/idehub/GoogleAnalyticsBridge/GoogleAnalyticsBridgePackage.java
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.google.android.gms.tagmanager.ContainerHolder;
+//import com.google.android.gms.tagmanager.ContainerHolder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,20 +13,16 @@ import java.util.List;
 
 public class GoogleAnalyticsBridgePackage implements ReactPackage {
 
-    public GoogleAnalyticsBridgePackage(ContainerHolder containerHolder, String trackingId) {
-        _containerHolder = containerHolder;
+    public GoogleAnalyticsBridgePackage(String trackingId) {
+//        _containerHolder = containerHolder;
         _trackingId = trackingId;
     }
 
-    public GoogleAnalyticsBridgePackage(String trackingId) {
-        this(null, trackingId);
-    }
-
     public GoogleAnalyticsBridgePackage() {
-        this(null, null);
+        this(null);
     }
 
-    private ContainerHolder _containerHolder = null;
+    //    private ContainerHolder _containerHolder = null;
     private String _trackingId;
 
     @Override
@@ -35,7 +31,7 @@ public class GoogleAnalyticsBridgePackage implements ReactPackage {
         List<NativeModule> modules = new ArrayList<>();
 
         modules.add(new GoogleAnalyticsBridge(reactContext, _trackingId));
-        modules.add(new GoogleTagManagerBridge(reactContext, _containerHolder));
+//        modules.add(new GoogleTagManagerBridge(reactContext, _containerHolder));
 
         return modules;
     }
--- android/src/main/java/com/idehub/GoogleAnalyticsBridge/GoogleTagManagerBridge.java
+++ android/src/main/java/com/idehub/GoogleAnalyticsBridge/GoogleTagManagerBridge.java
@@ -8,9 +8,9 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.tagmanager.ContainerHolder;
-import com.google.android.gms.tagmanager.DataLayer;
-import com.google.android.gms.tagmanager.TagManager;
+//import com.google.android.gms.tagmanager.ContainerHolder;
+//import com.google.android.gms.tagmanager.DataLayer;
+//import com.google.android.gms.tagmanager.TagManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,9 +18,9 @@ import java.util.concurrent.TimeUnit;
 
 public class GoogleTagManagerBridge extends ReactContextBaseJavaModule {
 
-    public GoogleTagManagerBridge(ReactApplicationContext reactContext, ContainerHolder containerHolder) {
+    public GoogleTagManagerBridge(ReactApplicationContext reactContext) {
         super(reactContext);
-        mContainerHolder = containerHolder;
+//        mContainerHolder = containerHolder;
     }
 
 
@@ -30,9 +30,9 @@ public class GoogleTagManagerBridge extends ReactContextBaseJavaModule {
     private final String E_OPEN_CONTAINER_FAILED = "E_OPEN_CONTAINER_FAILED";
     private final String E_PUSH_EVENT_FAILED = "E_PUSH_EVENT_FAILED";
 
-    private ContainerHolder mContainerHolder;
+    //    private ContainerHolder mContainerHolder;
     private Boolean openOperationInProgress = false;
-    private DataLayer mDatalayer;
+//    private DataLayer mDatalayer;
 
     @Override
     public String getName() {
@@ -41,81 +41,81 @@ public class GoogleTagManagerBridge extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void openContainerWithId(final String containerId, final Promise promise){
-        if (mContainerHolder != null) {
-            promise.reject(E_CONTAINER_ALREADY_OPEN, new Throwable("The container is already open."));
-            return;
-        }
-
-        if (openOperationInProgress) {
-            promise.reject(E_ONGOING_OPEN_OPERATION, new Throwable("Container open-operation already in progress."));
-            return;
-        }
-
-        TagManager mTagManager = TagManager.getInstance(getReactApplicationContext());
-        //using -1 here because it can't access raw in app
-        openOperationInProgress = true;
-        PendingResult<ContainerHolder> pending = mTagManager.loadContainerPreferFresh(containerId, -1);
-        pending.setResultCallback(new ResultCallback<ContainerHolder>() {
-            @Override
-            public void onResult(ContainerHolder containerHolder) {
-                if (containerHolder != null && containerHolder.getStatus().isSuccess()) {
-                    mContainerHolder = containerHolder;
-                    promise.resolve(true);
-                } else {
-                    promise.reject(E_OPEN_CONTAINER_FAILED, new Throwable(String.format("Failed to open container. Does container with id %s exist?", containerId)));
-                }
-                openOperationInProgress = false;
-            }
-        }, 2000, TimeUnit.MILLISECONDS);
+//        if (mContainerHolder != null) {
+//            promise.reject(E_CONTAINER_ALREADY_OPEN, new Throwable("The container is already open."));
+//            return;
+//        }
+//
+//        if (openOperationInProgress) {
+//            promise.reject(E_ONGOING_OPEN_OPERATION, new Throwable("Container open-operation already in progress."));
+//            return;
+//        }
+//
+//        TagManager mTagManager = TagManager.getInstance(getReactApplicationContext());
+//        //using -1 here because it can't access raw in app
+//        openOperationInProgress = true;
+//        PendingResult<ContainerHolder> pending = mTagManager.loadContainerPreferFresh(containerId, -1);
+//        pending.setResultCallback(new ResultCallback<ContainerHolder>() {
+//            @Override
+//            public void onResult(ContainerHolder containerHolder) {
+//                if (containerHolder != null && containerHolder.getStatus().isSuccess()) {
+//                    mContainerHolder = containerHolder;
+//                    promise.resolve(true);
+//                } else {
+//                    promise.reject(E_OPEN_CONTAINER_FAILED, new Throwable(String.format("Failed to open container. Does container with id %s exist?", containerId)));
+//                }
+//                openOperationInProgress = false;
+//            }
+//        }, 2000, TimeUnit.MILLISECONDS);
     }
 
     @ReactMethod
     public void booleanForKey(final String key, final Promise promise){
-        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
-            promise.resolve(mContainerHolder.getContainer().getBoolean(key));
-        } else {
-            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
-        }
+//        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
+//            promise.resolve(mContainerHolder.getContainer().getBoolean(key));
+//        } else {
+//            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
+//        }
     }
 
     @ReactMethod
     public void stringForKey(final String key, final Promise promise){
-        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
-            promise.resolve(mContainerHolder.getContainer().getString(key));
-        } else {
-            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
-        }
+//        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
+//            promise.resolve(mContainerHolder.getContainer().getString(key));
+//        } else {
+//            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
+//        }
     }
 
     @ReactMethod
     public void doubleForKey(final String key, final Promise promise){
-        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
-            promise.resolve(mContainerHolder.getContainer().getDouble(key));
-        } else {
-            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
-        }
+//        if (mContainerHolder != null && mContainerHolder.getContainer() != null) {
+//            promise.resolve(mContainerHolder.getContainer().getDouble(key));
+//        } else {
+//            promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
+//        }
     }
 
     @ReactMethod
     public void pushDataLayerEvent(ReadableMap dictionary, final Promise promise){
 
-      if (mContainerHolder != null && isValidMapToPushEvent(dictionary)) {
-          getDataLayer().push(ConvertReadableToMap.getMap(dictionary));
-          promise.resolve(true);
-      } else {
-          if (mContainerHolder == null) {
-              promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
-          } else {
-              promise.reject(E_PUSH_EVENT_FAILED, new Throwable("Validation error, data must have a key \"event\" with valid event name"));
-          }
-      }
+//      if (mContainerHolder != null && isValidMapToPushEvent(dictionary)) {
+//          getDataLayer().push(ConvertReadableToMap.getMap(dictionary));
+//          promise.resolve(true);
+//      } else {
+//          if (mContainerHolder == null) {
+//              promise.reject(E_CONTAINER_NOT_OPENED, new Throwable("The container has not been opened. You must call openContainerWithId(..)"));
+//          } else {
+//              promise.reject(E_PUSH_EVENT_FAILED, new Throwable("Validation error, data must have a key \"event\" with valid event name"));
+//          }
+//      }
     }
-    
+
     @ReactMethod
     public void setVerboseLoggingEnabled(final Boolean enabled, final Promise promise){
-        TagManager mTagManager = TagManager.getInstance(getReactApplicationContext());
-        mTagManager.setVerboseLoggingEnabled(enabled);
-        promise.resolve(true);
+//        TagManager mTagManager = TagManager.getInstance(getReactApplicationContext());
+//        mTagManager.setVerboseLoggingEnabled(enabled);
+//        promise.resolve(true);
     }
 
     private boolean isValidMapToPushEvent(ReadableMap dictionary) {
@@ -123,13 +123,13 @@ public class GoogleTagManagerBridge extends ReactContextBaseJavaModule {
                 && dictionary.getString("event").length() > 0);
     }
 
-    private DataLayer getDataLayer() {
-        if (mDatalayer == null) {
-            TagManager tagManager = TagManager.getInstance(getReactApplicationContext());
-            mDatalayer = tagManager.getDataLayer();
-        }
-        return mDatalayer;
-    }
+//    private DataLayer getDataLayer() {
+//        if (mDatalayer == null) {
+//            TagManager tagManager = TagManager.getInstance(getReactApplicationContext());
+//            mDatalayer = tagManager.getDataLayer();
+//        }
+//        return mDatalayer;
+//    }
 
 
 
